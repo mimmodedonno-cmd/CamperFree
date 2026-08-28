ca3cbcb65b8bfccb0930c38c4b8c38c7a5ae2c71
mimmodedonno-cmd <mimmodedonno@gmail.com>
2026-08-28 17:01:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,10 @@
-const CACHE='camperfree-pwa-v1.5.2';
-self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
+const CACHE='camperfree-1.6';
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
